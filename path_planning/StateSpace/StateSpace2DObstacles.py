21f1a7d6ae0285f9352d38e0c26555ac123c07e9
Andre Nguyen <andre-phu-van.nguyen@polymtl.ca>
2016-12-26 03:31:14 -0500

    almost got it

--- path_planning/StateSpace/StateSpace2DObstacles.py
+++ path_planning/StateSpace/StateSpace2DObstacles.py
@@ -1,8 +1,14 @@
 import random
+from enum import Enum
 import numpy as np
 import kdtree as KdTree
 from StateSpace.States import Point2D
 
+
+class MapState(Enum):
+    FREE = 0
+    OCCUPIED = 1
+    UNKNOWN = 2
 
 class StateSpace2DObstacles:
     """
@@ -19,30 +25,81 @@ class StateSpace2DObstacles:
 
     @property
     def width(self):
-        return map.shape[1]
+        return self.map.shape[1]
 
     @property
     def height(self):
-        return map.shape[0]
+        return self.map.shape[0]
 
     def random_state(self):
-        valid = False
-        while not valid:
-            rand_x = random.uniform(-1, 1) * self.width / 2
-            rand_y = random.uniform(-1, 1) * self.height / 2
-            state = Point2D(rand_x, rand_y)
-            if self.is_state_valid(state):
-                valid = True
+  #      valid = False
+ #       while not valid:
+        rand_x = int(random.uniform(0, 1) * self.width)
+        rand_y = int(random.uniform(0, 1) * self.height)
+        state = Point2D(rand_x, rand_y)
+#            if self.is_state_valid(state):
+#                valid = True
         return state
 
     def intermediate_state(self, source, target, step_size):
-        return
+        delta = target - source
+        delta = delta / np.linalg.norm(delta)
+        #np.true_divide(delta, np.linalg.norm(delta), delta, casting='unsafe')
+        state = source.vec + delta * step_size
+        state = state.astype(int)
+        return Point2D(state[0], state[1])
 
     def distance(self, source, target):
-        return
+        return np.linalg.norm(target - source)
 
     def is_state_valid(self, state):
-        return
+        in_bounds = False
+        if ((state.x >= 0) and (state.x <= self.width) and
+                (state.y >= 0) and (state.y <= self.height)):
+            in_bounds = True
+        else:
+            return False
+
+        safe = False
+        if self.map[state.x][state.y] == int(MapState.FREE.value):
+            safe = True
+
+        return (safe and in_bounds)
 
     def is_transition_valid(self, source, goal):
-        return
+        """
+        Checks for collisions when transitioning from source to goal
+        in a straight line using Bressenham line rasterization
+        https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+        :param source: Source state (Point2D)
+        :param goal: Goal state (Point2D)
+        :return: True or False
+        """
+        if (not self.is_state_valid(source)) or (not self.is_state_valid(goal)):
+            return False
+        x0, y0 = int(source.x), int(source.y)
+        x1, y1 = int(goal.x), int(goal.y)
+
+        dx = abs(x0 - x1)
+        dy = abs(y0 - y1)
+        if dx != 0.0: # not a vertical line
+            derr = abs(dy/dx)
+            err  = derr - 0.5
+            y = source.y
+            for x in range(x0, x1):
+                # check collision
+                if self.map[x][y] != int(MapState.FREE.value):
+                    return False
+                err += derr
+                if err >= 0.5:
+                    y += 1
+                    err -= 1.0
+            # Yay no collision found!
+            return True
+        else:
+            # is a vertical line, ezpz
+            for y in range(y0, y1):
+                if self.map[x0][y] != int(MapState.FREE.value):
+                    return False
+            return True
+
--- path_planning/main.py
+++ path_planning/main.py
@@ -1,7 +1,10 @@
 from StateSpace.StateSpace2D import StateSpace2D
+from StateSpace.StateSpace2DObstacles import StateSpace2DObstacles, MapState
 from StateSpace.States import Point2D
 from Tree import Tree
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from matplotlib.collections import LineCollection
 from scipy import misc as m
 """
@@ -43,12 +46,36 @@ def main():
     plt.waitforbuttonpress()
 
 def main_obstacles():
-    map = m.imread('Untitled.bmp')
+    map = m.imread('Untitled.bmp', flatten=True)
     # Binarize the map and the invert it
+    #map = np.ones([100,100])
     map[map>0] = 2
-    map[map==0] = 1
-    map[map==2] = 0
+    map[map==0] = MapState.OCCUPIED.value
+    map[map==2] = MapState.FREE.value
+    state_space = StateSpace2DObstacles(map)
+    tree = Tree(state_space)
+    tree.step_size = 2
+    tree.goal_max_dist = 1
+    tree.goal_bias = 0.2
+    tree.start_state = Point2D(0, 0)
+    tree.goal_state = Point2D(50, 50)
+    result = tree.run()
+    print('Found path to goal.' if result else 'Unable to find path to goal')
+    path = []
+    tree.getPath(path, tree.last_node)
+    segments = []
+    treeTraversal(tree.nodes[0], segments)
 
+    lc = LineCollection(segments, linestyles='solid')
+    ax = plt.axes()
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.add_collection(lc)
+    ax.set_title('Path')
+    plt.grid(True)
+    #plt.imshow(map, cmap="gray")
+    plt.show()
+    plt.waitforbuttonpress()
 
 if __name__ == '__main__':
     main_obstacles()
